2725625a6be4dddf3366341cc2508ac7f5116a3a
matth0128 <matth0128@gmail.com>
2016-02-22 22:03:59 -0600

    More adjustments to the weather page.

--- js/weather.js
+++ js/weather.js
@@ -7,15 +7,19 @@ function getWeather() {
     //Load Config Values
     configData = loadConfig();
     
+    //Current Timestamp
+        var timestamp = moment(weather.updated);
+
     $.simpleWeather({
     location: configData.weather.location,
     unit: 'f',
     success: function(weather) {
       console.dir(weather);
-      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html = '<h3>'+weather.city+', '+weather.region+'</h3>';
+      html += '<ul><li>'+weather.temp+'&deg;'+weather.units.temp+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.text+'</li></ul>';
+      html += '<p class="weather-update">Weather updated at '+moment(timestamp).format('MM/DD/YY h:mma')+'</p>';
   
       $("#weather").html(html);
     },
