17101a8b81dde94b1d2845d8279bb1f777658772
matth0128 <matth0128@gmail.com>
2016-02-22 21:40:34 -0600

    Adding additional data and config values to the "example_config.js" file.

--- js/example_config.js
+++ js/example_config.js
@@ -1,17 +1,24 @@
 // ***** Function loadConfig() ***** //
-// This function will return the basic configuration values. //
 function loadConfig(){
-    var config_data = {};
+    var configData = {};
     //Google Maps API Key & Link
-    config_data.google_maps_api_key = "YOUR API KEY";
-    config_data.google_maps_api_link = "https://maps.googleapis.com/maps/api/js?key="+config_data.google_maps_api_key+"&callback=initMap&signed_in=true";
-        
+    configData.googleMap = {};
+        configData.googleMap.APIKey = "YOUR API KEY";
+        configData.googleMap.APILink = "https://maps.googleapis.com/maps/api/js?key="+configData.googleMap.APIKey+"&callback=initMap&signed_in=true";
+        configData.googleMap.lat = 30.221048;
+        configData.googleMap.lng = -97.754094;
+        configData.googleMap.zoom = 11;
+    
+    //Default Weather Location ["City, ST"]
+    configData.weather = {};
+    configData.weather.location = "Buda, TX";
+
     //Return Data
-    return config_data;
+    return configData;
 }
 
 // ***** Function getGoogleMapLink() ***** //
 function getGoogleMapLink() {
-    config_data = loadConfig();
-    return "https://maps.googleapis.com/maps/api/js?key="+config_data.google_maps_api_key+"&callback=initMap&signed_in=true";
+    configData = loadConfig();
+    return "https://maps.googleapis.com/maps/api/js?key="+configData.googleMap.APIKey+"&callback=initMap&signed_in=true";
 }
