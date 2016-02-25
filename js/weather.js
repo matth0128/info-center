3a2dd57ac98f7fcb8903b6b838f34710a4b61e71
matth0128 <matth0128@gmail.com>
2016-02-24 18:16:07 -0600

    Additional style and functional updates to the weather page. It now has a dynamic system for filling in the weather values. It uses the "data-wx" custom attribute to select and insert the weather data value.

--- js/weather.js
+++ js/weather.js
@@ -7,29 +7,16 @@ function getWeather() {
     //Load Config Values
     configData = loadConfig();
 
-    //Current Timestamp
-    var timestamp = moment(weather.updated);
-
     $.simpleWeather({
         location: configData.weather.location,
         unit: 'f',
         success: function(weather) {
             console.dir(weather);
-            
-            $("#location").html(weather.city+' '+weather.region);
-            $("#currentWeather").html(weather.currently);
-            $("#temp").html(weather.temp+'&deg;'+weather.units.temp);
-            $("#currentDetails").html(weather.text);
-            $("#lastUpdate").html('<p class="weather-update">Weather updated at '+moment(timestamp).format('MM/DD/YY h:mma')+'</p>');
-
-            /*
-            html = '<h3>'+weather.city+', '+weather.region+'</h3>';
-            html += '<ul><li>'+weather.temp+'&deg;'+weather.units.temp+'</li>';
-            html += '<li class="currently">'+weather.currently+'</li>';
-            html += '<li>'+weather.text+'</li></ul>';
-            html += '<p class="weather-update">Weather updated at '+moment(timestamp).format('MM/DD/YY h:mma')+'</p>';
-            $("#weather").html(html);
-            */
+            //Inject Weather Data
+            $(".data-wx").each(function (){
+                wxValue = $(this).attr('data-wx');
+                $(this).html(weather[wxValue]);
+            });
         },
         error: function(error) {
             $("#weather").html('<p>'+error+'</p>');
