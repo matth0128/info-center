25b344e58704713801c4160fd9da499204b46a34
matth0128 <matth0128@gmail.com>
2016-02-28 21:40:40 -0600

    First version of the weather page complete. This is just a simple first step to get the weather page data on the page.

--- js/weather.js
+++ js/weather.js
@@ -9,26 +9,39 @@ function getWeather() {
 
     $.simpleWeather({
         location: configData.weather.location,
-        unit: "f",
-        success: function(weather) {
-            console.dir(weather);
-            //Inject Weather Data
-            $(".data-wx").each(function (){
-                wxValue = $(this).attr("data-wx");
-                if(wxValue.indexOf(".") >= 0){
-                    var value = weather;
-                    var strings = wxValue.split(".");
-                    for(i = 0; i < strings.length; i++){
-                        value = value[strings[i]];
-                    }
-                    $(this).html(value);
-                }else{
-                    $(this).html(weather[wxValue]);
-                }
-            });
-        },
-        error: function(error) {
-            $("#weather").html("<p>"+error+"</p>");
-        }
+      unit: 'f',
+      success: function(weather) {
+          console.dir(weather);
+          //Inject Weather Data
+          $('.data-wx').each(function (){
+              wxValue = $(this).attr('data-wx');
+              if(wxValue.indexOf('.') >= 0){
+                  var value = weather;
+                  var strings = wxValue.split('.');
+                  for(i = 0; i < strings.length; i++){
+                      value = value[strings[i]];
+                  }
+                  $(this).html(value);
+              }else{
+                  $(this).html(weather[wxValue]);
+              }
+          });
+          //Inject Current Conditions Image
+          $("#currentConditionsImage").html('<img src="'+weather.image+'">');
+          //Inject Forecast Data
+          var forecast_html;
+          for(i = 0; i < weather.forecast.length; i++){
+              forecast_data = weather.forecast[i];
+              forecast_html = forecast_data.date+'<br>';
+              forecast_html += forecast_data.day+'<br>';
+              forecast_html += forecast_data.high+' /'+forecast_data.low+'<br>';
+              forecast_html += '<img src="'+forecast_data.thumbnail+'"><br>';
+              forecast_html += forecast_data.text;
+              $('#forecast'+i).html(forecast_html);
+          }
+      },
+      error: function(error) {
+          $('#weather').html('<p>'+error+'</p>');
+      }
     });
 }
