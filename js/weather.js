13aaa307688b79ac04ee7f9658f8bca550395199
matth0128 <matth0128@gmail.com>
2016-04-09 20:21:04 -0500

    Creating the convertYahooWeatherCode() function in the weather.js file. Changing the name of the current conditions image section to "currentConditionsIcon" since the system is now using the Weather Icons library.

--- js/weather.js
+++ js/weather.js
@@ -27,7 +27,7 @@ function getWeather() {
               }
           });
           //Inject Current Conditions Image
-          $("#currentConditionsImage").html('<img src="'+weather.image+'">');
+          $("#currentConditionsIcon").addClass(convertYahooWeatherCode(weather.code));
           //Inject Daylight Duration
           var sunrise = moment(weather.sunrise, "hh:mm A");
           var sunset = moment(weather.sunset, "hh:mm A");
@@ -39,7 +39,6 @@ function getWeather() {
           //Inject Wind Speed/Direction Icon
           var wind_direction = weather.wind.direction.toLowerCase();
           $("#windSpeedDirection").addClass("wi-from-"+wind_direction);
-
           //Inject Forecast Data
           var forecast_html;
           for(i = 0; i < weather.forecast.length; i++){
@@ -47,7 +46,7 @@ function getWeather() {
               forecast_html = forecast_data.date+'<br>';
               forecast_html += forecast_data.day+'<br>';
               forecast_html += forecast_data.high+' /'+forecast_data.low+'<br>';
-              forecast_html += '<img src="'+forecast_data.thumbnail+'"><br>';
+              forecast_html += '<i class="wi '+convertYahooWeatherCode(forecast_data.code)+'"></i><br>';
               forecast_html += forecast_data.text;
               $('#forecast'+i).html(forecast_html);
           }
@@ -56,4 +55,61 @@ function getWeather() {
           $('#weather').html('<p>'+error+'</p>');
       }
     });
+}
+
+// Conver Yahoo Weather Code
+//***** Convert the Yahoo Weather API code to the corresponding Weather Icon. *****
+function convertYahooWeatherCode(code) {
+    switch(code){
+        case "0": return "wi-tornado"; //tornado
+        case "1": return "wi-hurricane"; //tropical storm 
+        case "2": return "wi-hurricane"; //hurricane
+        case "3": return "wi-thunderstorm"; //severe thunderstorms
+        case "4": return "wi-storm-showers"; //thunderstorm
+        case "5": return "wi-rain-mix"; //mixed rain and snow
+        case "6": return "wi-rain-mix"; //mixed rain and sleet
+        case "7": return "wi-rain-mix"; //mixed snow and sleet
+        case "8": return "wi-rain-mix"; //freezing drizzle
+        case "9": return "wi-sprinkle"; //drizzle
+        case "10": return "wi-windy"; //freezing rain
+        case "11": return "wi-showers"; //showsers
+        case "12": return "wi-showers"; //showsers
+        case "13": return "wi-snow"; //snow flurries
+        case "14": return "wi-snow"; //light snow showers
+        case "15": return "wi-snow-wind"; //blowing snow
+        case "16": return "wi-snow"; //snow
+        case "17": return "wi-hail"; //hail
+        case "18": return "wi-sleet"; //sleet
+        case "19": return "wi-dust"; //dust
+        case "20": return "wi-fog"; //foggy
+        case "21": return "wi-dust"; //haze
+        case "22": return "wi-smoke"; //smoke
+        case "23": return "wi-strong-wind"; //blustery
+        case "24": return "wi-strong-wind"; //windy
+        case "25": return "wi-snowflake-cold"; //cold
+        case "26": return "wi-cloudy"; //cloudy
+        case "27": return "wi-night-alt-cloudy"; //mostly cloudy (night)
+        case "28": return "wi-day-cloudy"; //mostly cloudy (day)
+        case "29": return "wi-night-alt-cloudy"; //partly cloudy (night)
+        case "30": return "wi-day-cloudy"; //partly cloudy (day)
+        case "31": return "wi-night-clear"; //clear (night)
+        case "32": return "wi-day-sunny"; //sunny
+        case "33": return "wi-night-clear"; //fair (night)
+        case "34": return "wi-day-sunny"; //fair (day)
+        case "35": return "wi-hail"; //mixed rain and hail
+        case "36": return "wi-day-sunny"; //hot
+        case "37": return "wi-thunderstorm"; //isolated thunderstorms
+        case "38": return "wi-storm-showers"; //scattered thunderstorms
+        case "39": return "wi-storm-showers"; //scattered thunderstorms
+        case "40": return "wi-showers"; //scattered showers
+        case "41": return "wi-snow"; //heavy snow
+        case "42": return "wi-snow"; //scattered show showers
+        case "43": return "wi-snow"; //heavy snow
+        case "44": return "wi-cloudy"; //partly cloudy
+        case "45": return "wi-stom-showers"; //thundershowers
+        case "46": return "wi-snow"; //snow showers
+        case "47": return "wi-storm-showers"; //isolated thundershowers
+        case "3200": return "wi-na"; //not available
+        default: return "wi-na";
+    }
 }
