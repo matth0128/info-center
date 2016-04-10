f29f32e0e57f0ca21d4629b393b4ad684eb88b04
matth0128 <matth0128@gmail.com>
2016-04-10 13:21:55 -0500

    Fixing the indentation in the "weather.js" file.

--- js/weather.js
+++ js/weather.js
@@ -9,51 +9,51 @@ function getWeather() {
 
     $.simpleWeather({
         location: configData.weather.location,
-      unit: 'f',
-      success: function(weather) {
-          console.dir(weather);
-          //Inject Weather Data
-          $('.data-wx').each(function (){
-              wxValue = $(this).attr('data-wx');
-              if(wxValue.indexOf('.') >= 0){
-                  var value = weather;
-                  var strings = wxValue.split('.');
-                  for(i = 0; i < strings.length; i++){
-                      value = value[strings[i]];
-                  }
-                  $(this).html(value);
-              }else{
-                  $(this).html(weather[wxValue]);
-              }
-          });
-          //Inject Current Conditions Image
-          $("#currentConditionsIcon").addClass(convertYahooWeatherCode(weather.code));
-          //Inject Daylight Duration
-          var sunrise = moment(weather.sunrise, "hh:mm A");
-          var sunset = moment(weather.sunset, "hh:mm A");
-          var duration = moment.duration(sunset.diff(sunrise));
-          var duration_hour = Math.floor(duration.asHours());
-          var duration_min = Math.floor(duration.asMinutes()) - (duration_hour * 60);
-          var hours = duration_hour+"h "+duration_min+"m";
-          $('#dayDuration').html(hours);
-          //Inject Wind Speed/Direction Icon
-          var wind_direction = weather.wind.direction.toLowerCase();
-          $("#windSpeedDirection").addClass("wi-towards-"+wind_direction);
-          //Inject Forecast Data
-          var forecast_html;
-          for(i = 0; i < weather.forecast.length; i++){
-              forecast_data = weather.forecast[i];
-              forecast_html = forecast_data.date+'<br>';
-              forecast_html += forecast_data.day+'<br>';
-              forecast_html += forecast_data.high+' /'+forecast_data.low+'<br>';
-              forecast_html += '<i class="wi '+convertYahooWeatherCode(forecast_data.code)+'"></i><br>';
-              forecast_html += forecast_data.text;
-              $('#forecast'+i).html(forecast_html);
-          }
-      },
-      error: function(error) {
-          $('#weather').html('<p>'+error+'</p>');
-      }
+        unit: 'f',
+        success: function(weather) {
+            console.dir(weather);
+            //Inject Weather Data
+            $('.data-wx').each(function (){
+                wxValue = $(this).attr('data-wx');
+                if(wxValue.indexOf('.') >= 0){
+                    var value = weather;
+                    var strings = wxValue.split('.');
+                    for(i = 0; i < strings.length; i++){
+                        value = value[strings[i]];
+                    }
+                    $(this).html(value);
+                }else{
+                    $(this).html(weather[wxValue]);
+                }
+            });
+            //Inject Current Conditions Image
+            $("#currentConditionsIcon").addClass(convertYahooWeatherCode(weather.code));
+            //Inject Daylight Duration
+            var sunrise = moment(weather.sunrise, "hh:mm A");
+            var sunset = moment(weather.sunset, "hh:mm A");
+            var duration = moment.duration(sunset.diff(sunrise));
+            var duration_hour = Math.floor(duration.asHours());
+            var duration_min = Math.floor(duration.asMinutes()) - (duration_hour * 60);
+            var hours = duration_hour+"h "+duration_min+"m";
+            $('#dayDuration').html(hours);
+            //Inject Wind Speed/Direction Icon
+            var wind_direction = weather.wind.direction.toLowerCase();
+            $("#windSpeedDirection").addClass("wi-towards-"+wind_direction);
+            //Inject Forecast Data
+            var forecast_html;
+            for(i = 0; i < weather.forecast.length; i++){
+                forecast_data = weather.forecast[i];
+                forecast_html = forecast_data.date+'<br>';
+                forecast_html += forecast_data.day+'<br>';
+                forecast_html += forecast_data.high+' /'+forecast_data.low+'<br>';
+                forecast_html += '<i class="wi '+convertYahooWeatherCode(forecast_data.code)+'"></i><br>';
+                forecast_html += forecast_data.text;
+                $('#forecast'+i).html(forecast_html);
+            }
+        },
+        error: function(error) {
+            $('#weather').html('<p>'+error+'</p>');
+        }
     });
 }
 
